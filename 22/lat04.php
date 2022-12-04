6bcb759a5080ad754f26e44d2fd89401a3770465
zasfiaclaudia <112447707+zasfiaclaudia@users.noreply.github.com>
2022-12-04 08:20:44 +0700

    Add files via upload

---
+++ 22/lat04.php
@@ -0,0 +1,55 @@
+<?php 
+
+    //OPERATOR MATEMATIKA
+
+    $a = 2;
+    $b = 2;
+
+    $c = $a + $b;
+
+    echo $c. '<br>';
+
+    $c = $a - $b;
+    echo $c. '<br>';
+
+    $c = $a * $b;
+    echo $c. '<br>';
+
+    $c = $a / $b;
+    echo floor ($c). '<br>';
+
+    $c = $a % $b;
+    echo $c. '<br><br>';
+
+    //OPERATOR LOGIKA
+
+    $c = $a < $b;
+    echo $c;
+
+    $c = $a > $b;
+    echo $c;
+
+    $c = $a == $b;
+    echo $c;
+
+    $c = $a != $b;
+    echo $c. '<br><br>';
+
+    // INCREMENT
+
+    $a++;
+    echo $a. '<br><br>';
+
+    //OPERATOR STRING
+
+    $kata = 'Sura';
+    $kota = 'baya';
+
+    $hasil = $kata.$kota;
+
+    $hasil .=' KOTA PAHLAWAN';
+    echo $hasil;
+
+
+
+?>
---
+++ 22/restoran/kategori/update.php
@@ -0,0 +1,44 @@
+<?php 
+
+    require_once "../function.php";
+
+    $sql = "SELECT * FROM tblkategori WHERE idkategori = $id";
+    $result = mysqli_query($koneksi, $sql);
+
+    $row = mysqli_fetch_assoc($result);
+
+    // $kategori = 'Mie Ayam';
+    // $id = 12;
+    // $sql = "UPDATE tblkategori SET kategori = '$kategori' WHERE idkategori = $id";
+
+    // $result = mysqli_query($koneksi, $sql);
+
+
+    // echo $sql;
+
+
+
+
+?>
+
+<form action="" method="post">
+    kategori :
+    <input type="text" name="kategori" value="<?php echo $row['kategori']?>">
+    <br>
+    <input type="submit" name="simpan" value="Simpan">
+</form>
+
+<?php 
+
+    if (isset($_POST['simpan'])) {
+
+        $kategori = $_POST['kategori'];
+        
+        $sql = "UPDATE tblkategori SET kategori = '$kategori' WHERE idkategori = $id";
+
+        $result = mysqli_query($koneksi, $sql);
+        
+        header("location:http://localhost/PHP-SMK/22/restoran/kategori/select.php");
+    }
+
+?>
